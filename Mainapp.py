7aecba1b6e98c031efd8d6e242bda51c19fe49ff
CAUSTIC-cpu <victormakenzi12@gmail.com>
2025-05-03 15:44:40 +0300

    Update Mainapp.py

--- Mainapp.py
+++ Mainapp.py
@@ -1,4 +1,8 @@
-import streamlit as st import pandas as pd import numpy as np import matplotlib.pyplot as plt import datetime
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import datetime
 
 --- Mock Data Generator ---
 
